2081fc9c13d8b3c5eca7084d1549a14034ae475b
JSDenis <den.sanches@mail.ru>
2024-11-23 15:05:37 +0300

    commit success

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,7 +5,7 @@ describe('GET /datalab6', () => {
     it('should return Lab 6 data.', async () => {
         const response = await request(app).get('/datalab6');
         expect(response.status).toBe(200);
-        expect(response.text).toBe('Lab 6 data.');
+        expect(response.text).toBe('Lab 6 data.123');
     });
 });
  
