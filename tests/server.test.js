3e5d1d8d8cdf5195a9ec4b259204253fd2a927a9
JSDenis <32075762+JSDenis@users.noreply.github.com>
2025-10-25 05:55:32 +0300

    Update server.test.js

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('GET /datalab6', () => {
-    it('should return Lab 6 data.', async () => {
+    it('should return Lab 5 data.', async () => {
         const response = await request(app).get('/datalab6');
         expect(response.status).toBe(200);
         expect(response.text).toBe('Lab 6 data.123');
