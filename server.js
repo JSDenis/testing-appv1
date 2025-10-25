e53e5f6157d02dd29a0756cde32905a1afdfc1bb
JSDenis <32075762+JSDenis@users.noreply.github.com>
2025-10-25 05:56:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/datalab6', (req, res) => {
+app.get('/datalab5', (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.send('Lab 6 data.123');
+    res.send('Lab 5 data.123');
 });
 
 if (require.main === module) {
